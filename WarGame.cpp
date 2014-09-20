86bee758d29b7715c3b2a53becaa4bcac151f6a7
Andrew <GonzalezAJ1@gcc.edu>
2014-09-20 18:40:43 -0400

    Create WarGame.cpp

---
+++ WarGame.cpp
@@ -0,0 +1,16 @@
+/* 
+Author: Class Warfare
+Course: COMP 220, Computer Programming II
+Date: 09/25/14
+Description: The gameplay file.
+*/
+
+#include "War.h"
+
+int main()
+{
+  War::play();
+  
+	return 0;
+}
+
