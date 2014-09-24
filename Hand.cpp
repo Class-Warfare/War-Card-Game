23cabdae479650166efdab32890ef27c29bf1d40
Addison D <DenchikAM1@users.noreply.github.com>
2014-09-24 17:53:53 -0400

    Sync Error Fix
    Fixing sync conflict for placeTop naming error.

--- Hand.cpp
+++ Hand.cpp
@@ -73,13 +73,8 @@ Card Hand::drawBottom()
 
 void Hand::placeTop(Card c)
 {
-<<<<<<< HEAD
 	//Insert the card at the beginning of the deck
 	cards.insert(cards.begin(), c);
-=======
-    //Insert the card at the beginning of the deck
-	cards.insert(cards.begin(), card);
->>>>>>> origin/master
 }
 
 void Hand::placeBottom(Card c)
